48ec94e16a3fe339df1830af22e6bb4a2885de88
Robert Romano <bromano1992@gmail.com>
2022-07-26 09:29:45 -0400

    Updated sendreq to work with axios

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import api from '../utilities/calls';
+import instance from '../utilities/calls';
 import baseURL from '../utilities/calls';
 import headers from '../utilities/calls';
 
@@ -10,9 +10,9 @@ export default function MenuItems() {
 
 	useEffect(function () {
 		(async function getMenuItems() {
-      const result = await api.get('/menuitems/')
-      console.log(result)
-      setMenuItems(result)
+      const res = await instance('/menuitems/')
+      console.log(res.data)
+      // setMenuItems(result)
 		})();
 	}, []);
 
--- src/utilities/calls.js
+++ src/utilities/calls.js
@@ -1,14 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-// baseURL = 'http://localhost:8000/api/'
-
-const api = async () => {
-	axios.create({
-		baseURL: 'http://localhost:8000/api',
-		headers: {
-			'Content-type': 'application/json',
-		},
-	});
-};
-
-export default api;
+export default async function sendRequest(url, method = 'GET', payload = null) {
+	// Fetch takes an optional options object as the 2nd argument
+	// used to include a data payload, set headers, etc.
+	const options = { method };
+	if (payload) {
+		options.headers = { 'Content-Type': 'application/json' };
+		options.body = JSON.stringify(payload);
+	}
+	const res = await axios(url, options);
+	if (res.ok) return res.json();
+	// res.ok will be false if the status code set to 4xx in the controller action
+	throw new Error('Bad Request');
+}
