93ffe0d01c6e9a021e3471ac6b6ba129ae0e8a22
Robert Romano <bromano1992@gmail.com>
2022-07-26 14:03:11 -0400

    Separated menu item data into containers on page

--- src/components/MenuItemList.jsx
+++ src/components/MenuItemList.jsx
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from 'react';
 // import MenuListItem from './MenuListItem';
 
 export default function MenuItems({ menuItems }) {
-  console.log({ menuItems })
-  
-  
-    
-  const menu = menuItems.map((item, index) => {
-        console.log(item);
-      })
-    
-      
-    return (
-    <div>{menu}</div>
-    )
-  }
+	console.log({ menuItems });
+
+
+	return (
+		<div>
+      {menuItems.map((item, index) => (
+        <>
+          <div className='item-container'>
+				  <h1>{item.name}</h1>
+          <p>{item.description}</p>
+            <p>${item.price}</p>
+            {/* <img src={item.image} alt="" /> */}
+          </div>
+          </>
+			))}
+		</div>
+	);
+}
