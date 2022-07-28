6e733bcb28c45c1786afb1dcb326df683696e1ff
Robert Romano <bromano1992@gmail.com>
2022-07-27 22:56:11 -0400

    refactored sendrequest

---
+++ src/utilities/sendrequest.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+export default async function sendRequest(url, method = 'GET', payload = null) {
+	let data;
+
+	const options = { method };
+	// if (payload) {
+	//     options.headers = { 'Content-Type': 'application/json' };
+	//     options.data = payload;
+	//   }
+
+	// let data;
+	const res = await axios(url, options);
+	// console.log(res.data)
+	return res.data;
+	// if (res.ok)
+	// throw new Error('Bad Request');
+	
+}
+export async function sendRequestAPI(url, method = 'GET', payload = null) {
+	let data;
+
+	const options = { method };
+	// if (payload) {
+	//     options.headers = { 'Content-Type': 'application/json' };
+	//     options.data = payload;
+	//   }
+
+	// let data;
+
+	const res = await axios(url, options);
+	// console.log(res.data)
+	return res.data;
+	// if (res.ok)
+	// throw new Error('Bad Request');
+}
